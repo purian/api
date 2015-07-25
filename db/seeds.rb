47dc8b307e354b87cc363202484e6593d83e2639
purian <purian@gmail.com>
2015-07-25 16:49:13 +0300

    Generate 1000 products records

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+
+# Option for predefined categories
+# CATEGORY = ["Beverages", "Condiments", "Confections", "Dairy Products", "Grains/Cereals", "Meat/Poultry", "Produce", "Seafood"] # Took from Northwind DB
+
+
+# Secure way yo generate random alphanumeric strings
+# sku_array = [] # Should insert here the generate sku after add it to a product record
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# def generate_sku
+#   self.token = loop do
+#     random_token = Array.new(6){[*'0'..'9', *'A'..'Z'].sample}.join
+#     break random_token unless sku_array.include?(random_token)
+#   end
+# end
+
+
+# Drop products if exists
+if Product.all.size > 0
+  puts # Just for make the display prettier
+  puts # Just for make the display prettier
+  puts "Deleting all Products..."
+  Product.delete_all
+end
+puts # Just for make the display prettier
+puts # Just for make the display prettier
+puts "Importing Products.."
+progressbar = ProgressBar.create(:title => "Products", starting_at: 0, total: 1000)
+1000.times.each do |index|
+  sku = index.to_s.rjust(4, '0') + Array.new(4){[*'0'..'9', *'A'..'Z'].sample}.join
+  Product.create(name: Faker::Commerce.product_name, sku: sku, category: Faker::Commerce.department)
+  progressbar.increment
+end
